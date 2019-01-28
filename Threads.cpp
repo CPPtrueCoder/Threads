71f1893415d4659f1106fa44969f5b39da494b20
andierodin@gmail.com <Thewayto1>
2019-01-29 02:52:22 +0300

    Working with threads

--- Threads.cpp
+++ Threads.cpp
@@ -1,34 +1,35 @@
+#include <chrono>
 #include <iostream>
 #include <thread>
-#include <chrono>
 
 using namespace std;
 
-void DoWork (int &a);
+int Sum(int a, int b);
 
-int main()
-{
-  int q = 5 ;
+int main() {
+  int result;
+  thread t ([&result] () { result = Sum(2, 5); });
 
-  thread t  (DoWork, ref(q));
 cout << "Hello World!" << endl;
-for (size_t i = 0 ; i<10; ++i)
-{
-  cout<< "ID of thread = "<<this_thread::get_id()<<"\tmain works"<<i<<endl;
-  this_thread::sleep_for(chrono ::milliseconds (500));
+for (size_t i = 0; i < 10; ++i) {
+  cout << "ID of thread = " << this_thread::get_id() << "\tmain works" << i
+       << endl;
+  this_thread::sleep_for(chrono ::milliseconds(500));
 }
-
-cout<< q<<endl;
 t.join();
+cout<< "Sum Result =  " <<result << endl;
 return 0;
 }
 
-void DoWork (int &a){
+int Sum(int a, int b) {
 
-  this_thread::sleep_for(chrono::milliseconds (2000));
-  cout<< "ID of thread " << this_thread::get_id()<<"========\tDowork STARTED\t========";
+  this_thread::sleep_for(chrono::milliseconds(2000));
+  cout << "ID of thread " << this_thread::get_id()
+       << " ========\tDowork STARTED\t========";
 
-  this_thread::sleep_for(chrono::milliseconds(1000));
-  a*=2;
-  cout<< "ID of thread " << this_thread::get_id()<<"========\tDowork STOOPWS\t========";
+  this_thread::sleep_for(chrono::milliseconds(5000));
+
+  cout << "ID of thread " << this_thread::get_id()
+       << "========\tDowork STOOPWS\t========"<<endl;
+  return a + b;
 }
